57708f53fc9cb5cb0d26ff45c0f7a01170538a95
mendelor <33259548+mendelor@users.noreply.github.com>
2020-01-24 23:37:52 +1300

    Create SampleTest.java

---
+++ SampleTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test; 
+
+Public class SampleTest {
+   
+     @Test
+     public void simpleTest() {
+        int a = 1;
+        int b = 2; 
+        assertTrue(a + b == 3);
+    
+     }
+}
